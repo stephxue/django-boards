611ecc365bdc18727c2121b8c043216dd0b700c7
Stephanie <stephanie_66_1997@hotmail.com>
2018-05-28 18:04:33 +0800

    Removed database_url from settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -146,8 +146,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
